c4d7f0099188a0b805d092d5c72c63ca6bc0cd6d
fredmcnab <frederickmcnab@trentu.ca>
2022-10-31 14:42:55 -0400

    Added CardNumber class to Core, modified Home page to add functionality to "Create" button

---
+++ COIS-3850H_Project/Core/CardNumber.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateCardWPFPractice
+{
+    public class CardNumber
+    {
+        Random rnd = new Random();
+        private int issNum = 7;
+        private int random69;
+        private int cardLength;
+        private int accnum;
+
+        //initialize array to be maximum needed size (16)
+        //find end of array before adding check dig?
+
+
+        //private int[] card = new int[16];
+        private int[] card = new int[16];
+
+
+        //Constructor for card int[]
+        //predetermined values for issuer# and bank# in card[0-5]
+        //6 - 9 random Account Number digits in card[6 - cardLength -1]
+        //Check digit (of 9 for now) in card[cardlength-1] (last digit)
+        public int[] Card
+        {
+            get
+            {
+                card[0] = issNum;
+                card[1] = 5;
+                card[2] = 5;
+                card[3] = 5;
+                card[4] = 5;
+                card[5] = 5;
+
+                for (int i = 6; i < CardLength - 2; i++)
+                {
+                    card[i] = AccNum;
+                }
+
+                //last digit = check digit(9)
+
+                card[CardLength] = 9;
+                return card;
+            }
+        }
+
+        //Random number from 6 - 9 to determine number of random account number digits
+        public int Random69
+        {
+            get
+            {
+                int random69 = rnd.Next(6, 9);
+                return random69;
+            }
+        }
+
+        //Card Length property
+        //1 IssNum + 5 BankNum Digits + 1 Check Digit = 7 + random 6 - 9 accountNum digits
+        public int CardLength
+        {
+            get { return Random69 + 7; }
+        }
+
+        //Constructor for random account number
+        //generate and return random int from 0 - 10
+        public int AccNum
+        {
+            get
+            {
+                int accnum = rnd.Next(10);
+                return accnum;
+            }
+        }
+
+    }
+}
--- COIS-3850H_Project/MVVM/View/HomePage.xaml.cs
+++ COIS-3850H_Project/MVVM/View/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenerateCardWPFPractice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,8 @@ namespace COIS_3850H_Project.MVVM.View
             InitializeComponent();
         }
 
+
+
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -45,6 +48,35 @@ namespace COIS_3850H_Project.MVVM.View
             LoginPage loginPage = new LoginPage();
             loginPage.Show();
             this.Close();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //new CardNumber object
+            CardNumber testCard = new CardNumber();
+
+            //Converting Card Digits into one String to display to User:
+            //first half of Card Number to String: "7 55555"
+            string a = string.Concat(testCard.Card[0], " ", testCard.Card[1], testCard.Card[2], testCard.Card[3], testCard.Card[4], testCard.Card[5], " ");
+
+            //second half of Card Number to String
+            //start at first account number digit, add each following account number digit to string
+            string b = Convert.ToString(testCard.Card[6]);
+
+            for (int i = 7; i < testCard.CardLength - 1; i++)
+            {
+                b = b + Convert.ToString(testCard.Card[i]);
+            }
+
+            //check dig
+            string c = string.Concat(" ", testCard.Card[testCard.CardLength]);
+
+            //combine the above strings / parts of card number into one String "ccNumber"
+            string ccNumber = a + b + c;
+            //Console.WriteLine(ccNumber);
+            //Console.ReadLine();
+
+            MessageBox.Show(string.Format("Your Card Number is: \r\n{0}", ccNumber));
         }
     }
 }
