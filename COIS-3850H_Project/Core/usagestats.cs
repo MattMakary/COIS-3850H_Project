071e88ff8d5c49cdac95018aa51f60cfe7457f20
Blinky <matthewmakary@trentu.ca>
2022-11-07 17:19:21 -0500

    Card Number & Usage Stats

---
+++ COIS-3850H_Project/Core/usagestats.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COIS_3850H_Project.Core
+{
+    public class usagestats
+    {
+        private int monthlyAmount;
+        private int yearlyAmount;
+
+        public usagestats()
+        {
+            monthlyAmount = 0;
+            yearlyAmount = 0;
+        }
+    }
+}
+        
